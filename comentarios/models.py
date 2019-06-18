bd7f408002e8ad595bf6a18c2a8b56d9976138f6
johnatasr <johnatasr@hotmail.com>
2019-06-18 11:37:05 -0300

    add commit

---
+++ comentarios/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Comentario(models.Model):
+
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+    descricao = models.TextField()
+    data = models.DateField(auto_now_add=True)
+    aprovado = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.usuario.username
+
